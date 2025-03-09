1afa508550c0eb5a9ea93d1a45fceb0f8df067c5
Linuun <jlenonninolla31@gmail.com>
2025-03-09 23:41:37 +0800

    added pseudocode

---
+++ input_10_numbers_print_sum.py
@@ -0,0 +1,6 @@
+# initialize an empty set to store numbers
+# initialize a variable to store the sum
+# ask user to input 10 numbers
+# use for loop
+# add the number being input to the sum
+# print sum
