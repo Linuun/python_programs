789bf50e5315e63565682f11da88c71a42dbbd5e
Linuun <jlenonninolla31@gmail.com>
2025-03-09 23:07:42 +0800

    if-else

--- input_2_numbers_print_equal_when_same.py
+++ input_2_numbers_print_equal_when_same.py
@@ -2,4 +2,8 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 # check if the numbers are equal
+if num1 == num2:
+    print("The numbers are equal")
 # if the numbers are not equal
+else:
+    print("The numbers are not equal")
