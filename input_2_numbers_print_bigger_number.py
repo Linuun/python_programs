7609cea8099481187715b35c8411e40cbceb5a8a
Linuun <jlenonninolla31@gmail.com>
2025-03-09 22:58:12 +0800

    user input

--- input_2_numbers_print_bigger_number.py
+++ input_2_numbers_print_bigger_number.py
@@ -1,5 +1,8 @@
 # ask user to input 2 numbers 
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 # check if num1 is greater than num2
 # check if they are equal
 # check if num2 is greater than num1
+
 
