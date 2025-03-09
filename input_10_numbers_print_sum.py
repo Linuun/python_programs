bb3b7738b4eab515215aaf0241abf00413ca9e58
Linuun <jlenonninolla31@gmail.com>
2025-03-09 23:46:44 +0800

    for loop

--- input_10_numbers_print_sum.py
+++ input_10_numbers_print_sum.py
@@ -5,5 +5,8 @@ sum = 0
 # ask user to input 10 numbers
 print("Enter 10 numbers: ")
 # use for loop
+for i in range(10):
+    num = int(input(f"Enter number {i+1}: "))
+    numbers.append(num)
 # add the number being input to the sum
 # print sum
