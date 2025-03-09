e78f75d1ff986732b20c29686edef47f5c9556ef
Linuun <jlenonninolla31@gmail.com>
2025-03-09 23:09:18 +0800

    removed the space after print

--- input_2_numbers_print_bigger_number.py
+++ input_2_numbers_print_bigger_number.py
@@ -3,12 +3,12 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 # check if num1 is greater than num2
 if num1 > num2:
-    print (f"The bigger number is: {num1}")
+    print(f"The bigger number is: {num1}")
 # check if they are equal
 elif num1 == num2 :
-    print ("They are equal")
+    print("They are equal")
 # check if num2 is greater than num1
 else:
-    print (f"The bigger number is: {num2}")
+    print(f"The bigger number is: {num2}")
 
 
