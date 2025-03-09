7bd889db9c4891258cfe51960c4bf5a5ae2e49ce
Linuun <jlenonninolla31@gmail.com>
2025-03-09 23:43:43 +0800

    initialize variable to store sum

--- input_10_numbers_print_sum.py
+++ input_10_numbers_print_sum.py
@@ -1,6 +1,7 @@
 # initialize an empty set to store numbers
 numbers = []
 # initialize a variable to store the sum
+sum = 0
 # ask user to input 10 numbers
 # use for loop
 # add the number being input to the sum
