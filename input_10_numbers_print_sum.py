cf969d5402184fc18bf66037c197b5a23552f73b
Linuun <jlenonninolla31@gmail.com>
2025-03-09 23:44:32 +0800

    user input

--- input_10_numbers_print_sum.py
+++ input_10_numbers_print_sum.py
@@ -3,6 +3,7 @@ numbers = []
 # initialize a variable to store the sum
 sum = 0
 # ask user to input 10 numbers
+print("Enter 10 numbers: ")
 # use for loop
 # add the number being input to the sum
 # print sum
