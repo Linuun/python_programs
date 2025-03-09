f76f7e85640bc402ae836d74af5509d1e4d7864b
Linuun <jlenonninolla31@gmail.com>
2025-03-09 23:54:03 +0800

    added pseudocode

---
+++ input_10_numbers_print_odd.py
@@ -0,0 +1,7 @@
+# initialize an empty set to store numbers
+# initialize a variable to store odd numbers
+# ask user to input 10 numbers
+# use for loop
+# process of knowing if a number is odd
+# add 1 if the number is odd
+# print the total number of odd number/s
