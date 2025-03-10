87e57757c34de0954a14c1a552c1a9ac2e4160a2
Linuun <jlenonninolla31@gmail.com>
2025-03-10 22:38:55 +0800

    added pseudocode

---
+++ batch_2/input_10_numbers_print_first_number_minus_remaining_numbers.py
@@ -0,0 +1,6 @@
+# initialize an empty set to store numbers
+# use for loop
+# ask user to input 10 numbers
+# assigned the first number to a variable
+# process of subtracting the first number to the remaining
+# print result
