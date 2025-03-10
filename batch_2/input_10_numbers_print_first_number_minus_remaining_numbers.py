a983c83f6ef70388c4c265a952d5d8ade4e0dcc0
Linuun <jlenonninolla31@gmail.com>
2025-03-10 22:40:17 +0800

    for loop

--- batch_2/input_10_numbers_print_first_number_minus_remaining_numbers.py
+++ batch_2/input_10_numbers_print_first_number_minus_remaining_numbers.py
@@ -1,6 +1,7 @@
 # initialize an empty set to store numbers
 numbers = []
 # use for loop
+for i in range(10):
 # ask user to input 10 numbers
 # assigned the first number to a variable
 # process of subtracting the first number to the remaining
