fb94bccb8984a975097be6f0bea3707cd0307b1f
Linuun <jlenonninolla31@gmail.com>
2025-03-10 22:42:12 +0800

    user input

--- batch_2/input_10_numbers_print_first_number_minus_remaining_numbers.py
+++ batch_2/input_10_numbers_print_first_number_minus_remaining_numbers.py
@@ -3,6 +3,8 @@ numbers = []
 # use for loop
 for i in range(10):
 # ask user to input 10 numbers
+    num = int(input(f"Enter a number {i+1}: "))
+    numbers.append(num)
 # assigned the first number to a variable
 # process of subtracting the first number to the remaining
 # print result
