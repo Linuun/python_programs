f155d4d70214e3fed5b53815504642408be703cc
Linuun <jlenonninolla31@gmail.com>
2025-03-10 22:20:44 +0800

    check if num2 is equal to zero

--- batch_2/input_2_numbers_print_quotient_without_decimal.py
+++ batch_2/input_2_numbers_print_quotient_without_decimal.py
@@ -2,5 +2,7 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 # check if num2 is equal to zero
+if num2 == 0:
+    print("Error: Division by zero is not allowed")
 # process of getting the quotient
 # print quotient
