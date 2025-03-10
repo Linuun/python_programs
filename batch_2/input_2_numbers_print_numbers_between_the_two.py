4ade2cfb18c3ce23a116f34e9e84e5aa3b907027
Linuun <jlenonninolla31@gmail.com>
2025-03-10 23:26:46 +0800

    check if num1 is less than num2

--- batch_2/input_2_numbers_print_numbers_between_the_two.py
+++ batch_2/input_2_numbers_print_numbers_between_the_two.py
@@ -2,6 +2,7 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 # check if num1 is less than num2
+if num1 < num2:
 # use for loop
 # print result
 # if num2 is less than num1 
