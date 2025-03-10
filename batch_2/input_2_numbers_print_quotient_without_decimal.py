f62428b9c1585c65a55fafef0a1b5a60500e54a9
Linuun <jlenonninolla31@gmail.com>
2025-03-10 22:19:12 +0800

    user input

--- batch_2/input_2_numbers_print_quotient_without_decimal.py
+++ batch_2/input_2_numbers_print_quotient_without_decimal.py
@@ -1,4 +1,6 @@
 # ask user to input 2 numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 # check if num2 is equal to zero
 # process of getting the quotient
 # print quotient
