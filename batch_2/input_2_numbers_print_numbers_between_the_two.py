79d5920fa0072b8337147114d48684a6e893e2e8
Linuun <jlenonninolla31@gmail.com>
2025-03-10 23:26:06 +0800

    user input

--- batch_2/input_2_numbers_print_numbers_between_the_two.py
+++ batch_2/input_2_numbers_print_numbers_between_the_two.py
@@ -1,4 +1,6 @@
 # ask user to input 2 numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 # check if num1 is less than num2
 # use for loop
 # print result
