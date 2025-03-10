427f41a547668debff5aaae15059fe1e0e167d07
Linuun <jlenonninolla31@gmail.com>
2025-03-10 22:59:00 +0800

    process of knowing if a number is even

--- batch_2/input_10_numbers_print_total_even_numbers.py
+++ batch_2/input_10_numbers_print_total_even_numbers.py
@@ -9,5 +9,6 @@ for i in range(10):
     num = int(input(f"Enter a number {i+1}: "))
     numbers.append(num)
 # process of knowing if a number is even
+    if num % 2 == 0:
 # add 1 if the number is even
 # print the total number of even number/s
