66e2e3dbbc7f674870cc2eb572cebd94aeb6dacd
Linuun <jlenonninolla31@gmail.com>
2025-03-10 21:54:03 +0800

    check if num1 is smaller than num2

--- batch_2/input_2_numbers_print_smaller_number.py
+++ batch_2/input_2_numbers_print_smaller_number.py
@@ -2,5 +2,7 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 # check if num1 is smaller than num2
+if num1 < num2:
+    print(f"The smaller number is: {num1}")
 # check if they are equal
 # check if num2 is smaller than num1
