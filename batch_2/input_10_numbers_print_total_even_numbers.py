09950398d5849e65f139d45354cb3e26ebd2702f
Linuun <jlenonninolla31@gmail.com>
2025-03-10 22:57:44 +0800

    for loop

--- batch_2/input_10_numbers_print_total_even_numbers.py
+++ batch_2/input_10_numbers_print_total_even_numbers.py
@@ -5,6 +5,9 @@ even = 0
 # ask user to input 10 numbers
 print("Enter 10 numbers: ")
 # use for loop
+for i in range(10):
+    num = int(input(f"Enter a number {i+1}: "))
+    numbers.append(num)
 # process of knowing if a number is even
 # add 1 if the number is even
 # print the total number of even number/s
