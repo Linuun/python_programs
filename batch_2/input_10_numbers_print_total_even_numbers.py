68f3cd3e0a111d842aca3c55438ce514c666863e
Linuun <jlenonninolla31@gmail.com>
2025-03-10 22:56:29 +0800

    user input

--- batch_2/input_10_numbers_print_total_even_numbers.py
+++ batch_2/input_10_numbers_print_total_even_numbers.py
@@ -3,6 +3,7 @@ numbers = []
 # initialize a variable to store even numbers
 even = 0
 # ask user to input 10 numbers
+print("Enter 10 numbers: ")
 # use for loop
 # process of knowing if a number is even
 # add 1 if the number is even
