26c71de62f5ef8df48fa5fde5cd5e5235a8f813e
Linuun <jlenonninolla31@gmail.com>
2025-03-10 22:54:28 +0800

    added pseudocode

---
+++ batch_2/input_10_numbers_print_total_even_numbers.py
@@ -0,0 +1,7 @@
+# initialize an emtpy set to store numbers
+# initialize a variable to store even numbers
+# ask user to input 10 numbers
+# use for loop
+# process of knowing if a number is even
+# add 1 if the number is even
+# print the total number of even number/s
