f8f2157cd0eccf2b35802a2fb0f82b213fe5d4d0
Linuun <jlenonninolla31@gmail.com>
2025-03-10 23:46:00 +0800

    added an error that if num2 is equal to zero, it will not proceed to division

--- batch_1/input_2_numbers_print_quotient_with_decimal.py
+++ batch_1/input_2_numbers_print_quotient_with_decimal.py
@@ -1,7 +1,11 @@
 # ask user to input 2 numbers
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
+# check if num2 is equal to zero
+if num2 == 0:
+    print("Error: Division by zero is not allowed")
 # process of getting the quotient
-quotient = num1 / num2
+else:
+    quotient = num1 / num2
 # print quotient
 print(quotient)
