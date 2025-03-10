29bacf11232a204d2c4cce7c02e0345756523c39
Linuun <jlenonninolla31@gmail.com>
2025-03-10 21:52:27 +0800

    user input

--- batch_2/input_2_numbers_print_smaller_number.py
+++ batch_2/input_2_numbers_print_smaller_number.py
@@ -1,4 +1,6 @@
 # ask user to input 2 numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 # check if num1 is smaller than num2
 # check if they are equal
 # check if num2 is smaller than num1
