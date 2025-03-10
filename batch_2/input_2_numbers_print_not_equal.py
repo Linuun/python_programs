15dcada07f9a0b9034bd458d3872b030caa26c37
Linuun <jlenonninolla31@gmail.com>
2025-03-10 22:07:00 +0800

    check if num1 is not equal to num2

--- batch_2/input_2_numbers_print_not_equal.py
+++ batch_2/input_2_numbers_print_not_equal.py
@@ -2,4 +2,6 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 # check if num1 is not equal to num2
+if num1 != num2:
+    print("The numbers are not equal")
 # else they are equal
