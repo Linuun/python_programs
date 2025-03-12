59118a5c7391aea954e99d3870fa80136401b077
Linuun <jlenonninolla31@gmail.com>
2025-03-12 16:56:36 +0800

    added pseudocode

---
+++ batch_4/input_number_until_invalid_display_average.py
@@ -0,0 +1,8 @@
+# initialize a variable for the sum and count
+# use while loop
+# use try block
+# ask the user to input number
+# process of getting the sum of the numbers
+# add 1 to the counter everytime a number is inputted
+# use except block to break the loop if the user inputs non-numeric
+# process of getting the average and display it
