065b50c9b7241ee24ab05990579f80a2b2d23f36
Linuun <jlenonninolla31@gmail.com>
2025-03-12 16:19:42 +0800

    use try block

--- batch_4/input_number_until_invalid_display_lowest_number.py
+++ batch_4/input_number_until_invalid_display_lowest_number.py
@@ -3,6 +3,7 @@ numbers = []
 # use while loop
 while True:
 # use try block
+    try:
 # ask user to input number
 # use except block to break the loop if the user inputs non-numeric
 # look for the highest number and display it
