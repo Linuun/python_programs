05e240023014d071277c2e77213a141c8503f1fb
Linuun <jlenonninolla31@gmail.com>
2025-03-12 16:17:49 +0800

    added pseudocode

---
+++ batch_4/input_number_until_invalid_display_lowest_number.py
@@ -0,0 +1,6 @@
+# initialize an empty set to store numbers
+# use while loop
+# use try block
+# ask user to input number
+# use except block to break the loop if the user inputs non-numeric
+# look for the highest number and display it
