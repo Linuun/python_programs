44157251a07eca955e54ba63ad5984112100d657
Linuun <jlenonninolla31@gmail.com>
2025-03-12 15:22:00 +0800

    user input

--- batch_3/input_number_until_invalid_display_lowest_number.py
+++ batch_3/input_number_until_invalid_display_lowest_number.py
@@ -5,5 +5,7 @@ while True:
 # use try block
     try:
 # ask user to input number
+        num = int(input("Enter a number (or any non-numeric to stop): "))
+        numbers.append(num)
 # use except block to break the loop if the user inputs non-numeric
 # check the lowest number in the list and display
