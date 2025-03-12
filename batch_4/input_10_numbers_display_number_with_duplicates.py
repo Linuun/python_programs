924d57d6465dafca465a04a0e8211895a161c1c1
Linuun <jlenonninolla31@gmail.com>
2025-03-12 15:50:47 +0800

    user input

--- batch_4/input_10_numbers_display_number_with_duplicates.py
+++ batch_4/input_10_numbers_display_number_with_duplicates.py
@@ -3,6 +3,7 @@ def find_duplicates():
 # initialize an empty set to store numbers
     numbers = []
 # ask user to input 10 numbers
+    print("Enter 10 numbers: ")
 # use for loop
 # check the numbers that appear more than once
 # check if there are duplicates and display the numbers with duplicates
