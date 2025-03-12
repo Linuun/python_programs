dcedad576ca76dc90a08f0d63acf14a6d861e78a
Linuun <jlenonninolla31@gmail.com>
2025-03-12 14:32:07 +0800

    initialize a set to track numbers that are entered by the user

--- batch_3/input_10_numbers_display_only_first_entry_for_duplicate.py
+++ batch_3/input_10_numbers_display_only_first_entry_for_duplicate.py
@@ -3,6 +3,7 @@ def get_numbers_without_duplicates():
 # initialize an empty set to store numbers
     numbers = []
 # initialize a set to track numbers that are entered
+    seen = set()
 # ask user to input 10 numbers
 # use for loop
 # check if there are duplicates
