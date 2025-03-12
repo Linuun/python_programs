7a9264ae1e66ade99fe2fda0680e8d8a67be80b0
Linuun <jlenonninolla31@gmail.com>
2025-03-12 15:18:47 +0800

    added pseudocode

---
+++ batch_3/input_number_until_invalid_display_lowest_number.py
@@ -0,0 +1,6 @@
+# initialize an empty set to store numbers
+# use while loop
+# use try block
+# ask user to input number
+# use except block to break the loop if the user inputs non-numeric
+# check the lowest number in the list and display
