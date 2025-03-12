b0d6d8bf3256ddaf5afca638a7bd6c1cea3290cd
Linuun <jlenonninolla31@gmail.com>
2025-03-12 16:00:51 +0800

    added pseudocode

---
+++ batch_4/input_number_until_invalid_display_number_with_most_duplicates.py
@@ -0,0 +1,6 @@
+# initialize an empty set to store numbers
+# use while loop
+# use try block
+# ask user to input a number
+# use except block to break the loop if the user inputs non-numeric
+# look for the number with most duplicates and display
