3ba6af05b44cb55b9dde86d163c6e214a00394f8
Linuun <jlenonninolla31@gmail.com>
2025-03-12 15:37:59 +0800

    use except block to break the loop if the user inputs non-numeric

--- batch_3/input_number_until_invalid_display_lowest_to_highest.py
+++ batch_3/input_number_until_invalid_display_lowest_to_highest.py
@@ -8,4 +8,6 @@ while True:
         num = int(input("Enter a number (or any non-numeric to stop): "))
         numbers.append(num)
 # use except block to break the loop if the user inputs non-numeric
+    except ValueError:
+        break
 # check if the list contains number and display it in ascending order
