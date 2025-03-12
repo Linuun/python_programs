bbc0bb46be2b36d73b86734970b4638e70bb2d06
Linuun <jlenonninolla31@gmail.com>
2025-03-12 14:29:16 +0800

    define a function

--- batch_3/input_10_numbers_display_only_first_entry_for_duplicate.py
+++ batch_3/input_10_numbers_display_only_first_entry_for_duplicate.py
@@ -1,4 +1,5 @@
 # define a function
+def get_numbers_without_duplicates():
 # initialize an empty set to store numbers
 # initialize a set to track numbers that are entered
 # ask user to input 10 numbers
