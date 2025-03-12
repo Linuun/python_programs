02e5174b9dc9ce259da929d3dabd35e891dfa648
Linuun <jlenonninolla31@gmail.com>
2025-03-12 14:30:04 +0800

    initialize an empty set to store numbers

--- batch_3/input_10_numbers_display_only_first_entry_for_duplicate.py
+++ batch_3/input_10_numbers_display_only_first_entry_for_duplicate.py
@@ -1,6 +1,7 @@
 # define a function
 def get_numbers_without_duplicates():
 # initialize an empty set to store numbers
+numbers = []
 # initialize a set to track numbers that are entered
 # ask user to input 10 numbers
 # use for loop
