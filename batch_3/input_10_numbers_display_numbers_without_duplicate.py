dab2ca6f263babdde7734169e31b8c9a76f0d796
Linuun <jlenonninolla31@gmail.com>
2025-03-12 14:08:57 +0800

    added pseudocode

---
+++ batch_3/input_10_numbers_display_numbers_without_duplicate.py
@@ -0,0 +1,6 @@
+# define a function
+# initialize an empty set to store numbers
+# ask user to input 10 numbers
+# use for loop
+# identify the numbers that appears only once
+# display unique numbers
