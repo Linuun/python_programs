334b4d698657978a5deacb3030897129e27de055
Linuun <jlenonninolla31@gmail.com>
2025-03-12 16:32:33 +0800

    added pseudocode

---
+++ batch_4/input_number_until_invalid_display_highest_to_lowest.py
@@ -0,0 +1,6 @@
+# initialize an empty set to store numbers
+# use while loop
+# use try block
+# ask user to input number
+# use except block to break the loop if the user inputs non-numeric
+# check if the list contains number and display in it descending order
