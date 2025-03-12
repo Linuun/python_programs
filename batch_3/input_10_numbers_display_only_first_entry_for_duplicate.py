1b210f7940e813b867da4a5791194269eee87ab8
Linuun <jlenonninolla31@gmail.com>
2025-03-12 14:28:05 +0800

    added pseudocode

---
+++ batch_3/input_10_numbers_display_only_first_entry_for_duplicate.py
@@ -0,0 +1,7 @@
+# define a function
+# initialize an empty set to store numbers
+# initialize a set to track numbers that are entered
+# ask user to input 10 numbers
+# use for loop
+# check if there are duplicates
+# display all numbers excluding duplicates
