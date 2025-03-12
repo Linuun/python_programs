62ba7d1fd2b1fc2080d0b909ad743070848d8795
Linuun <jlenonninolla31@gmail.com>
2025-03-12 14:11:54 +0800

    user input

--- batch_3/input_10_numbers_display_numbers_without_duplicate.py
+++ batch_3/input_10_numbers_display_numbers_without_duplicate.py
@@ -3,6 +3,7 @@ def get_unique_numbers():
 # initialize an empty set to store numbers
     numbers = []
 # ask user to input 10 numbers
+    print("Enter 10 numbers: ")
 # use for loop
 # identify the numbers that appears only once
 # display unique numbers
