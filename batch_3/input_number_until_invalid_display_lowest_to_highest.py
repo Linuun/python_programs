c18fcdc1038ce1d0a18c4d1115fe8fe7cf6cbeca
Linuun <jlenonninolla31@gmail.com>
2025-03-12 15:35:35 +0800

    use try block

--- batch_3/input_number_until_invalid_display_lowest_to_highest.py
+++ batch_3/input_number_until_invalid_display_lowest_to_highest.py
@@ -3,6 +3,7 @@ numbers = []
 # use while loop
 while True:
 # use try block
+    try: 
 # ask user to input a number
 # use except block to break the loop if the user inputs non-numeric
 # check if the list contains number and display it in ascending order
