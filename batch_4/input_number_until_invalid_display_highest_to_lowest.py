d5deef6770af408132c3f9e722a6d745170b057a
Linuun <jlenonninolla31@gmail.com>
2025-03-12 16:33:33 +0800

    while loop

--- batch_4/input_number_until_invalid_display_highest_to_lowest.py
+++ batch_4/input_number_until_invalid_display_highest_to_lowest.py
@@ -1,6 +1,7 @@
 # initialize an empty set to store numbers
 numbers = []
 # use while loop
+while True:
 # use try block
 # ask user to input number
 # use except block to break the loop if the user inputs non-numeric
