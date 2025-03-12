42ab5d95978b905801c5e9717c6399b342855abc
Linuun <jlenonninolla31@gmail.com>
2025-03-12 15:50:13 +0800

    initialize an empty set to store numbers

--- batch_4/input_10_numbers_display_number_with_duplicates.py
+++ batch_4/input_10_numbers_display_number_with_duplicates.py
@@ -1,6 +1,7 @@
 # define a function
 def find_duplicates():
 # initialize an empty set to store numbers
+    numbers = []
 # ask user to input 10 numbers
 # use for loop
 # check the numbers that appear more than once
