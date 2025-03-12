0c39b70ad2bcc391c6dd372835a1456771e354d3
Linuun <jlenonninolla31@gmail.com>
2025-03-12 14:32:46 +0800

    user input

--- batch_3/input_10_numbers_display_only_first_entry_for_duplicate.py
+++ batch_3/input_10_numbers_display_only_first_entry_for_duplicate.py
@@ -5,6 +5,7 @@ def get_numbers_without_duplicates():
 # initialize a set to track numbers that are entered
     seen = set()
 # ask user to input 10 numbers
+    print("Enter 10 numbers: ")
 # use for loop
 # check if there are duplicates
 # display all numbers excluding duplicates
