35a13ce91e1d605c900730762aa49763a62e8525
Linuun <jlenonninolla31@gmail.com>
2025-03-12 15:48:28 +0800

    added pseudocode

---
+++ batch_4/input_10_numbers_display_number_with_duplicates.py
@@ -0,0 +1,6 @@
+# define a function
+# initialize an empty set to store numbers
+# ask user to input 10 numbers
+# use for loop
+# check the numbers that appear more than once
+# check if there are duplicates and display the numbers with duplicates
