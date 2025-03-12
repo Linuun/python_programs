da01d82bab6fd242da28090967a7d902f04fd3b0
Linuun <jlenonninolla31@gmail.com>
2025-03-12 16:06:52 +0800

    added an append

--- batch_4/input_number_until_invalid_display_number_with_most_duplicates.py
+++ batch_4/input_number_until_invalid_display_number_with_most_duplicates.py
@@ -6,5 +6,6 @@ while True:
     try:
 # ask user to input a number
         num = int(input("Enter a number(or any non-numeric to stop): "))
+        numbers.append(num)
 # use except block to break the loop if the user inputs non-numeric
 # look for the number with most duplicates and display
