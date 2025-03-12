d3187cfc25d77969ad5aabd20f5229253e5fb9a8
Linuun <jlenonninolla31@gmail.com>
2025-03-12 15:20:23 +0800

    use try block

--- batch_3/input_number_until_invalid_display_lowest_number.py
+++ batch_3/input_number_until_invalid_display_lowest_number.py
@@ -3,6 +3,7 @@ numbers = []
 # use while loop
 while True:
 # use try block
+    try:
 # ask user to input number
 # use except block to break the loop if the user inputs non-numeric
 # check the lowest number in the list and display
