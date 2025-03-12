d63227bce86f57850a9eb35653f7f10ce7fd313c
Linuun <jlenonninolla31@gmail.com>
2025-03-12 16:58:20 +0800

    use try block

--- batch_4/input_number_until_invalid_display_average.py
+++ batch_4/input_number_until_invalid_display_average.py
@@ -4,6 +4,7 @@ count = 0
 # use while loop
 while True:
 # use try block
+    try:
 # ask the user to input number
 # process of getting the sum of the numbers
 # add 1 to the counter everytime a number is inputted
